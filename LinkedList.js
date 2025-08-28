1fbbae1453c673dc57ba0a2d45a1a939369e140c
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2025-08-28 18:12:52 -0400

    Implement `pop()` to erase last node

--- LinkedList.js
+++ LinkedList.js
@@ -44,12 +44,37 @@ export class LinkedList {
         this.#head = new Node(value, this.#head);
     }
 
+    pop() {
+        if(this.size === 0) return;
+
+        if(this.size === 1) {
+            this.#head = null;
+            return;
+        }
+        
+        let tmp = this.#head;
+        while(tmp.nextNode.nextNode != null) {
+            tmp = tmp.nextNode;
+        }
+        tmp.nextNode = null;
+
+    }
+
 }
 const list = new LinkedList();
 
 list.prepend("C");
 list.prepend("B");
 list.prepend("A");
+
+list.append("D");
+list.append("E");
+list.append("F");
+list.append("G");
+list.append("H");
+
+list.pop();
+list.pop();
 
 console.log("Head: ", list.head);
 console.log("Tail: ", list.tail);
